73e6670ecb3643d4e328f176ff01fa724a804de3
MOIOPaul <71466074+MOIOPaul@users.noreply.github.com>
2021-03-24 08:50:37 +0100

    Add files via upload

---
+++ _nuxt/static/1616572192/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FApiWp\u002F_nuxt\u002Fstatic\u002F1616572192",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{app:{basePath:"\u002FApiWp\u002F",assetsPath:"\u002FApiWp\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"03b6a508"}}}}(null));
